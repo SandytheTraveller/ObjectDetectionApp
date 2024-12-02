353f62781e1a5848a652fb4cf970e0a1a5e92f69
Aleksandra Karpova <alesksandrak@gmail.com>
2024-12-02 14:21:21 +0100

    app detects object on the camera

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 import cv2
 
 # OpenCV DNN
-# initialize Neural Network
+# --> Initialize Neural Network
 net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
-# define a model
-model = cv2.dnn.DetectionModel()
-model.setInputParams(size=(320, 320), )
+# define a model + pass a network to the model
+model = cv2.dnn.DetectionModel(net)
+model.setInputParams(size=(320, 320), scale=1/255) # bigger size means better precision on detection, but slower
 
-# initialize Web Camera
+# --> Initialize Web Camera
 # 0 takes the first camera, 1 - the second, 2 - the third, etc.
 cap = cv2.VideoCapture(0)
 
@@ -17,6 +17,18 @@ cap = cv2.VideoCapture(0)
 while True:
     # ret is saying if we have the frame (True) or not (False)
     ret, frame = cap.read()
+
+    # --> Object Detection
+    (class_ids, scores, bboxes) = model.detect(frame) # we get classes, scores and bounding boxes
+    for class_id, score, bbox in zip(class_ids, scores, bboxes):
+        x, y, width, height = bbox # coordinates - parameters - of the bounding box
+        # draw bounding boxes
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (200, 0, 50), 3) # where: frame; starting point; ending point; colour; thickness
+
+    print("Class ids: ", class_ids)
+    print("Scores: ", scores)
+    print("BBoxes: ", bboxes)
+
     # show the frame
     cv2.imshow("Frame", frame)
     # wait until the key is pressed to keep the frame open until told otherwise
