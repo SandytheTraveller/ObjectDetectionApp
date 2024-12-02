ded04fc2be4d60e906a1f5da1fae2bf23181471f
Aleksandra Karpova <alesksandrak@gmail.com>
2024-12-02 14:36:39 +0100

    added bouding boxes and classes ids

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cf
 model = cv2.dnn.DetectionModel(net)
 model.setInputParams(size=(320, 320), scale=1/255) # bigger size means better precision on detection, but slower
 
+# --> Load classes' list
+classes = []
+with open("dnn_model/classes.txt", "r") as file_object:
+    for class_name in file_object.readlines():
+        class_name = class_name.strip()
+        classes.append(class_name)
+
+
 # --> Initialize Web Camera
 # 0 takes the first camera, 1 - the second, 2 - the third, etc.
 cap = cv2.VideoCapture(0)
@@ -21,7 +29,8 @@ while True:
     # --> Object Detection
     (class_ids, scores, bboxes) = model.detect(frame) # we get classes, scores and bounding boxes
     for class_id, score, bbox in zip(class_ids, scores, bboxes):
-        x, y, width, height = bbox # coordinates - parameters - of the bounding box
+        (x, y, width, height) = bbox # coordinates - parameters - of the bounding box
+        cv2.putText(frame, str(class_id), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 0, 50), 2) # y-5, so that it's with the object but doesn't overlap with it
         # draw bounding boxes
         cv2.rectangle(frame, (x, y), (x + width, y + height), (200, 0, 50), 3) # where: frame; starting point; ending point; colour; thickness
 
